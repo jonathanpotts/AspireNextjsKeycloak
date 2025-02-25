340722e29686e0239e9336bbb774ce7ed6053395
Jonathan Potts <jonathanpotts@outlook.com>
2025-02-25 13:02:17 -0600

    Update to Aspire 9.1.0

--- AspireNextjsKeycloak.AppHost/Program.cs
+++ AspireNextjsKeycloak.AppHost/Program.cs
@@ -7,7 +7,7 @@ var keycloak = builder
     .WithDataVolume()
     .WithRealmImport("./KeycloakRealms");
 
-var keycloakRealm = "AspireNextjsKeycloak";
+const string keycloakRealm = "AspireNextjsKeycloak";
 
 var apiService = builder
     .AddProject<Projects.AspireNextjsKeycloak_ApiService>("apiservice")
@@ -16,17 +16,12 @@ var apiService = builder
     .WithReference(keycloak)
     .WaitFor(keycloak);
 
-// Workaround for https://github.com/dotnet/aspire/issues/7417
-// TODO: Update when fix comes out with Aspire 9.1
-var nextauthSecretValue = builder.Configuration["Parameters:nextauth-secret"];
-var nextauthSecret = !string.IsNullOrEmpty(nextauthSecretValue)
-    ? builder.AddParameter("nextauth-secret", nextauthSecretValue, secret: true)
-    : builder.AddParameter(
-        "nextauth-secret",
-        new GenerateParameterDefault { MinLength = 32, Special = false },
-        secret: true,
-        persist: true
-    );
+var nextauthSecret = builder.AddParameter(
+    "nextauth-secret",
+    new GenerateParameterDefault { MinLength = 32, Special = false },
+    secret: true,
+    persist: true
+);
 
 var webFrontend = builder
     .AddNpmApp("webfrontend", "../AspireNextjsKeycloak.Web", "dev")
