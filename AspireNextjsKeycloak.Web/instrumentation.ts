27ca2452de5b458edffb7c45dff94150ee7e9784
Jonathan Potts <jonathanpotts@outlook.com>
2025-04-18 11:53:40 -0500

    Update instrumentation.ts

--- AspireNextjsKeycloak.Web/instrumentation.ts
+++ AspireNextjsKeycloak.Web/instrumentation.ts
@@ -1,11 +1,10 @@
 import { registerOTel } from "@vercel/otel";
 
 export async function register() {
-  const serviceName = process.env.OTEL_SERVICE_NAME ?? "next-app";
-
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("./instrumentation.node");
   } else {
+    const serviceName = process.env.OTEL_SERVICE_NAME ?? "next-app";
     registerOTel({ serviceName });
   }
 }
