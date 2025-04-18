a38990d8f21e557774a0f3a28872f6901ea789ce
Jonathan Potts <jonathanpotts@outlook.com>
2025-04-18 11:11:20 -0500

    Remove unnecessary fetch when unauthorized and add WeatherForecast type

--- AspireNextjsKeycloak.Web/app/page.tsx
+++ AspireNextjsKeycloak.Web/app/page.tsx
@@ -15,28 +15,37 @@ import { getServiceEndpoint } from "@/service-discovery";
 import SignInButton from "./_components/sign-in-button";
 import SignOutButton from "./_components/sign-out-button";
 
+type WeatherForecast = {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary?: string;
+};
+
 export default async function Home() {
   const session = await auth();
-  const accessToken = session?.accessToken;
 
-  const res = await fetch(
-    `${getServiceEndpoint("apiservice")}/weatherforecast`,
-    {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
+  let error: string | undefined;
+  let data: WeatherForecast[] | undefined;
+
+  if (session) {
+    const accessToken = session?.accessToken;
+
+    const res = await fetch(
+      `${getServiceEndpoint("apiservice")}/weatherforecast`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
       },
-    },
-  );
+    );
 
-  const error = res.ok ? undefined : res.statusText;
-  const data = res.ok
-    ? ((await res.json()) as {
-        date: string;
-        temperatureC: number;
-        temperatureF: number;
-        summary: string;
-      }[])
-    : undefined;
+    if (!res.ok) {
+      error = res.statusText;
+    } else {
+      data = (await res.json()) as WeatherForecast[];
+    }
+  }
 
   return (
     <Container sx={{ py: 3 }}>
